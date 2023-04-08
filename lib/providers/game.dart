ece87a6bde668e2478e1f584eab761704e019c26
Arlan Dantas <arlanjose@gmail.com>
2023-04-07 23:37:31 -0300

    feat: Basic Moving

--- lib/providers/game.dart
+++ lib/providers/game.dart
@@ -13,8 +13,15 @@ class Game extends ChangeNotifier {
   late List<List<int>> boardData;
   late int cellsQty;
   Game({this.boardWidth = 4, this.boardHeight = 4}) {
-    boardData = List<List<int>>.filled(boardHeight, List.filled(boardWidth, 0));
+    boardData = List<List<int>>.generate(
+      boardHeight,
+      (_) => List<int>.generate(boardWidth, (_) => 0),
+    );
     cellsQty = boardHeight * boardWidth;
+    for (var i = 0; i < 5; i++) {
+      addNumber();
+    }
+    printBoard(boardData);
   }
 
   getCellValue({required int x, required int y}) {
@@ -34,13 +41,92 @@ class Game extends ChangeNotifier {
     do {
       x = random.nextInt(boardWidth);
       y = random.nextInt(boardHeight);
-    } while (getCellValue(x: x, y: y));
+    } while (getCellValue(x: x, y: y) != 0);
     int value = maxExponent != minExponent ? (minExponent + random.nextInt(maxExponent - minExponent)) : minExponent;
     setCellValue(x: x, y: y, value: value);
     ++filledNumbers;
   }
 
+  List<List<int>> getUnrotatedBoard(List<List<int>> board, Directions direction) {
+    Directions unrotateDirection = Directions.left;
+    switch (direction) {
+      case Directions.up:
+        unrotateDirection = Directions.down;
+        break;
+      case Directions.down:
+        unrotateDirection = Directions.up;
+        break;
+      case Directions.right:
+        unrotateDirection = Directions.right;
+        break;
+      default:
+    }
+    return getRotatedBoard(board, unrotateDirection);
+  }
+
+  List<List<int>> getRotatedBoard(List<List<int>> board, Directions direction) {
+    return List<List<int>>.generate(
+      boardHeight,
+      (i) => List.generate(boardWidth, (j) {
+        switch (direction) {
+          case Directions.up:
+            return board[j][boardHeight - 1 - i];
+          case Directions.down:
+            return board[boardWidth - 1 - j][i];
+          case Directions.right:
+            return board[boardHeight - 1 - i][boardWidth - 1 - j];
+          default:
+            return board[i][j];
+        }
+      }),
+    );
+  }
+
+  walkToBoard(Function(int i, int j) callback, {int minX = 0}) {
+    for (var i = 0; i < boardData.length; i++) {
+      for (var j = minX; j < boardData[i].length; j++) {
+        callback(i, j);
+      }
+    }
+  }
+
+  printBoard(List<List<int>> board) {
+    String ret = '';
+    walkToBoard((i, j) {
+      int v = board[i][j];
+      // ret += '${v < 10 ? '0' : ''}$v ';
+      ret += '$v ';
+      if (j == boardWidth - 1) {
+        ret += '\n';
+      }
+    });
+    print('\n$ret');
+  }
+
   move(Directions direction) {
     print("Moving to $direction");
+    var rotatedBoard = getRotatedBoard(boardData, direction);
+    walkToBoard((i, j) {
+      final int curValue = rotatedBoard[i][j] + 0;
+      if (curValue == 0) return;
+
+      if (rotatedBoard[i][j - 1] != 0) {
+        rotatedBoard[i][j - 1] += curValue;
+        rotatedBoard[i][j] = 0;
+        return;
+      }
+
+      rotatedBoard[i][j] = 0;
+      for (var k = j - 1; k > 0; k--) {
+        if (rotatedBoard[i][k] != 0) {
+          rotatedBoard[i][k + 1] = curValue;
+          return;
+        }
+      }
+      rotatedBoard[i][0] += curValue;
+    }, minX: 1);
+    boardData = getUnrotatedBoard(rotatedBoard, direction);
+    printBoard(boardData);
+    notifyListeners();
   }
 }
