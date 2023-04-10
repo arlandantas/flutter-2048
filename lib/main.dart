723260e512946d827ad4a38f5bef668d356fe3f9
Arlan Dantas <arlanjose@gmail.com>
2023-04-10 00:17:32 -0300

    build: Build web

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter 2048',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
       home: const GamePage(),
     );
